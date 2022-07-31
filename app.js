f6bfad789b46ef26cd8b68a93235ae2af0929995
juanfmange <juanfmange@gmail.com>
2022-07-31 03:01:23 -0500

    Learn to use tsc and tsc --watch

---
+++ app.js
@@ -0,0 +1,7 @@
+"use strict";
+const msg = 'hola mundo!';
+const persona = {
+    nombre: 'juan',
+    edad: 25,
+};
+console.log(persona.edad);
---
+++ app.ts
@@ -0,0 +1,7 @@
+
+const msg:string = 'hola mundo!';
+const persona = {
+    nombre: 'juan',
+    edad: 25,
+}
+console.log(persona.edad);
