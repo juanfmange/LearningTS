200756e3447b279826e0df51104acbb2d9bea0cd
Juan Fco. Mange <juanfmange@gmail.com>
2023-04-28 11:30:51 -0600

    strings

---
+++ tipos/string.ts
@@ -0,0 +1,13 @@
+(()=>{
+
+    const batman: string = 'Bat\'man';
+    const linternaVerde: string = "Linterna verde";
+    const volacanNegro: string = `Heroe: Volacan negro`;
+
+    const abc: number = 123;
+
+    console.log(abc.toString());
+
+    console.log(batman.toUpperCase());
+
+})()
