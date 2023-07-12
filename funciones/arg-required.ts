2dd7fcc42d5d9c8b45a9c72c8312ec3d4bc0e1f1
Juan Fco. Mange <juanfmange@gmail.com>
2023-07-12 14:34:11 -0600

    arg-required

---
+++ funciones/arg-required.ts
@@ -0,0 +1,12 @@
+(()=>{
+
+    const fullName = (firstName:string, lastName:(string|boolean)):string => {
+      return `${firstName} ${lastName}`
+  }
+
+    const name = fullName('Tony', 'Stark');
+
+    console.log({ name });
+    
+})()
+
