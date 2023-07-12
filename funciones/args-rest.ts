af84d4da90bec6e9cfc6a950bce5909eed20b4ef
Juan Fco. Mange <juanfmange@gmail.com>
2023-07-12 15:10:57 -0600

    arg-rest

---
+++ funciones/args-rest.ts
@@ -0,0 +1,10 @@
+(()=>{
+
+    const fullName = (firstName: string,...restArgs:string[]): string => {
+        return `${firstName}`
+        
+    }
+    const name = fullName('juan','mange','asu','json')
+    
+})()
+
