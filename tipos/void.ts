7036078626984c03619021a223f7e197e4aee662
Juan Fco. Mange <juanfmange@gmail.com>
2023-07-12 13:12:49 -0600

    void

---
+++ tipos/void.ts
@@ -0,0 +1,14 @@
+(()=>{
+
+    function callBatman():void {
+        
+    }
+    const a = callBatman()
+    console.log(a); // void = vacio --> undefined
+
+    const callSuperman = (): void => {
+        
+    }
+
+})()
+
