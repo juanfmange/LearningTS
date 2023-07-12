258c366fa4563de931933dce4b4c3f6222901961
Juan Fco. Mange <juanfmange@gmail.com>
2023-07-12 14:27:23 -0600

    funciones

---
+++ funciones/functions.ts
@@ -0,0 +1,16 @@
+(()=>{
+    const hero: string = 'flash';
+
+    const returnName = ():string => {
+        return hero;
+    }
+
+    const activar = () => {
+        
+        return 'hola';
+    }
+
+    console.log(typeof activar);
+
+})()
+
