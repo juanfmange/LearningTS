9186ad743bd6628891463d1d9ea504b3fceb2fa4
juanfmange <juanfmange@gmail.com>
2023-07-14 14:33:02 -0600

    types

---
+++ objetos/type.ts
@@ -0,0 +1,27 @@
+(() => {
+    
+    type Hero = {
+        name: string,
+        age?: number,
+        powers: string[],
+        getName?: () => string
+    }
+
+    let flash:Hero = {
+        name: 'Barry Allen',
+        age: 24,
+        powers: ['Super velocidad', 'Viajar en el tiempo']
+    }
+    
+    let superman:Hero = {
+        name: 'Clark kent',
+        age: 60,
+        powers: ['Super velocidad'],
+        getName() {
+            return this.name;
+        }
+    }
+
+
+
+})()
